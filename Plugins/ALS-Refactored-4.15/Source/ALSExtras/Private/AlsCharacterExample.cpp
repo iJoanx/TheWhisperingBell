9120c590899c05d02e14d6e44642e3978603f2d3
iJoanx <pyrosprout@gmail.com>
2025-05-22 15:42:34 +0200

    RunWalk

--- Plugins/ALS-Refactored-4.15/Source/ALSExtras/Private/AlsCharacterExample.cpp
+++ Plugins/ALS-Refactored-4.15/Source/ALSExtras/Private/AlsCharacterExample.cpp
@@ -116,7 +116,7 @@ void AAlsCharacterExample::Input_OnMove(const FInputActionValue& ActionValue)
 
 void AAlsCharacterExample::Input_OnSprint(const FInputActionValue& ActionValue)
 {
-	SetDesiredGait(ActionValue.Get<bool>() ? AlsGaitTags::Sprinting : AlsGaitTags::Walking);
+	SetDesiredGait(ActionValue.Get<bool>() ? AlsGaitTags::Running : AlsGaitTags::Walking);
 }
 
 void AAlsCharacterExample::Input_OnWalk()
