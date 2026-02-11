372c5340afd345e2cd55d70f0968677d6b1632e3
iJoanx <pyrosprout@gmail.com>
2026-02-11 14:06:47 +0100

    5.7

--- Source/TheWhisperingBellEditor.Target.cs
+++ Source/TheWhisperingBellEditor.Target.cs
@@ -8,7 +8,7 @@ public class TheWhisperingBellEditorTarget : TargetRules
 	public TheWhisperingBellEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 
 		ExtraModuleNames.AddRange( new string[] { "TheWhisperingBell" } );
 	}
